31bc58d3e38887d6a195319bd56e34c0218856e8
baby-developer <33032751+baby-developer@users.noreply.github.com>
2017-11-24 13:56:13 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exam-IN</title>
+	<link rel="stylesheet" type="text/css" href="lander-style.css">
+</head>
+<body>
+	<!--Navigation-->
+	<nav>
+		<div>
+			<button>Sign-in</button>
+			<button>Sign-Up</button>
+		</div>
+	</nav>
+	<!--Content-->
+
+
+
+	<!--Models-->
+	<div>
+		<div>
+			<h3>Sign-up</h3>
+			<input type="text" name="first" placeholder="First Name">
+			<input type="text" name="last" placeholder="Last Name">
+			<input type="email" name="email" placeholder="Email">
+			<input type="password" name="password" placeholder="Password">
+			<input type="password" name="re-password" placeholder="Re-Enter Password">
+			<button type="submit">Sign-up</button>
+			<button type="cancle">Close</button>
+		</div>
+	</div>
+
+	<div>
+		<div>
+			<h3>Sign-in</h3>
+			<input type="email" name="email" placeholder="Email">
+			<input type="password" name="password" placeholder="Password">
+			<button type="submit">Sign-in</button>
+			<button type="cancle">Close</button>
+		</div>
+	</div>
+
+	<!--Footer-->
+	<footer>
+		
+	</footer>
+</body>
+</html>
