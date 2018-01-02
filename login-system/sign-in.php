4a7003e63b78ca0a1af138392d2d15c8af429c71
Sujit Singh <33032751+baby-developer@users.noreply.github.com>
2018-01-02 13:00:59 +0530

    updated sign-in

--- login-system/sign-in.php
+++ login-system/sign-in.php
@@ -12,15 +12,15 @@ if ( $result->num_rows == 0 ){ // User doesn't exist
     header("location: ../error.php");
 }
 else { // User exists
-    
+
     $user = $result->fetch_assoc();
-    //Password Decrypt
 
-
-    if ( password_verify($_POST['password'], $user['password']) ) {
-        //Check wether the account has been verified or not
+    // Password verify
+    if ( password_verify($_POST['password'], $user['password'])) {
+        // Check wether the account has been verified or not
         if ($user['active'] == 1) {
-            //Initilize the session variables   
+            // Initilize the session variables
+            $_SESSION['user_id'] = $user['user_id'];
             $_SESSION['email'] = $user['email'];
             $_SESSION['first_name'] = $user['first_name'];
             $_SESSION['last_name'] = $user['last_name'];
@@ -31,21 +31,30 @@ else { // User exists
             // This is how we'll know the user is logged in
             $_SESSION['logged_in'] = true;
 
-            //Redirect to logged-in page
+            // Redirect to logged-in page
             if ($_SESSION['user_type'] == 0) {
+                $result_usertype = $conn->query("SELECT admin_id FROM admin WHERE user_id='{$_SESSION['user_id']}'");
+                $usertype = $result_usertype->fetch_assoc();
+                $_SESSION['admin_id'] = $usertype['admin_id'];
                 header("location: ../admin/admin_index.php");
             }
             elseif ($_SESSION['user_type'] == 1) {
+                $result_usertype = $conn->query("SELECT staff_id FROM staff WHERE user_id='{$_SESSION['user_id']}'");
+                $usertype = $result_usertype->fetch_assoc();
+                $_SESSION['staff_id'] = $usertype['staff_id'];
                 header("location: ../staff/staff_index.php");
             }
             elseif ($_SESSION['user_type'] == 2) {
+                $result_usertype = $conn->query("SELECT stud_id FROM student WHERE user_id='{$_SESSION['user_id']}'");
+                $usertype = $result_usertype->fetch_assoc();
+                $_SESSION['stud_id'] = $usertype['stud_id'];
                 header("location: ../student/student_index.php");
             }
         }
         else{
             $_SESSION['message'] = "Your account has not been verified please check your mail and activate your account";
             header("location: ../error.php");
-        }         
+        }
 
     }
     else {
@@ -53,5 +62,3 @@ else { // User exists
         header("location: ../error.php");
     }
 }
-
-
