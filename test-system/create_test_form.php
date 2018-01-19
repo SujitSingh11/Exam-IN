832a71a6efc3da53b7d1c831f298eacadfb631c3
Sujit Singh <33032751+baby-developer@users.noreply.github.com>
2018-01-19 21:31:01 +0530

    added file structure

--- test-system/create_test_form.php
+++ test-system/create_test_form.php
@@ -9,13 +9,29 @@
     <meta charset="utf-8">
     <title>Create Test</title>
     <?php  include '../include/css_include.php' ?>
+    <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
     <!--Sidenav-->
+    <nav>
+        <!--User Information-->
+
+        <!--Links-->
+
+        <!--Function Buttons-->
+    </nav>
 
     <!--Create Test Form-->
+    <div class="">
+
+    </div>
 
     <!--Footer-->
+    <footer>
+        <div class="footer-copyright">
+            <p>© 2018 Copyright Text </p>
+        </div>
+    </footer>
 
     <?php include '../include/js_include.php' ?>
   </body>
