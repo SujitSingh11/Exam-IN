7f7d1af4eaa893c7af56db9c0955020f1e7d8e99
Sujit Singh <33032751+baby-developer@users.noreply.github.com>
2018-02-04 21:00:44 +0530

    FontAwesome Updated

--- include/css_include.php
+++ include/css_include.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="shortcut icon" href="Logo-title.ico">
 <link rel="shortcut icon" href="../Logo-title.ico">
-<script defer src="https://use.fontawesome.com/releases/v5.0.3/js/all.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
