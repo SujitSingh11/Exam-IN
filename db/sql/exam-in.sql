e95c9246841f729b195f4a450c1e21302741b2f7
Sujit Singh <33032751+baby-developer@users.noreply.github.com>
2018-02-12 18:38:13 +0530

    new database

--- db/sql/exam-in.sql
+++ db/sql/exam-in.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2018 at 11:50 AM
+-- Generation Time: Feb 12, 2018 at 02:07 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -120,9 +120,9 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`stud_id`, `user_id`, `interest`) VALUES
-(4, 7, ''),
 (5, 9, ''),
-(6, 11, '');
+(6, 11, ''),
+(7, 12, '');
 
 -- --------------------------------------------------------
 
@@ -155,7 +155,22 @@ INSERT INTO `test_attempted` (`attempt_id`, `result_id`, `question_id`, `answer`
 (11, 3, 6, 1),
 (12, 3, 7, 1),
 (13, 3, 8, 1),
-(14, 3, 9, 1);
+(14, 3, 9, 1),
+(15, 4, 10, 3),
+(16, 4, 11, 2),
+(17, 4, 12, 4),
+(18, 4, 13, 1),
+(19, 4, 14, 2),
+(20, 5, 1, 1),
+(21, 5, 2, 3),
+(22, 5, 3, 1),
+(23, 5, 4, 4),
+(24, 5, 5, 0),
+(25, 6, 10, 3),
+(26, 6, 11, 2),
+(27, 6, 12, 4),
+(28, 6, 13, 0),
+(29, 6, 14, 1);
 
 -- --------------------------------------------------------
 
@@ -247,7 +262,10 @@ CREATE TABLE `test_result` (
 INSERT INTO `test_result` (`result_id`, `stud_id`, `test_id`, `attempted`, `not_attempted`, `right_answers`, `wrong_answers`, `marks_obtained`, `total_marks`) VALUES
 (1, 6, 7, 5, 0, 3, 2, 12, 19),
 (2, 6, 9, 5, 0, 2, 3, 5, 20),
-(3, 6, 8, 4, 0, 1, 3, -1, 14);
+(3, 6, 8, 4, 0, 1, 3, -1, 14),
+(4, 6, 9, 5, 0, 3, 2, 10, 20),
+(5, 6, 7, 4, 1, 4, 0, 14, 19),
+(6, 7, 9, 4, 1, 2, 2, 6, 20);
 
 -- --------------------------------------------------------
 
@@ -274,10 +292,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `username`, `email`, `password`, `hash`, `user_type`, `active`) VALUES
 (1, 'Sujit', 'Singh', 'Baby-Dev', 'sujitkumarsingh29@gmail.com', '$2y$10$R2z5mnHfRI.EL.1tXFdGBOZQ12SCZyKHi2IyO7cWEvd6GBlZoXn8S', '19f3cd308f1455b3fa09a282e0d496f4', 0, 1),
 (2, 'Shubham', 'Shripurkar', 'Bumquest', 'shirpurkar.shubham@gmail.com', '$2y$10$DWBo6pSsAIWwGRlFvIYUDO6mLGChMRigvtMyA.CnG2Hl1b3MROYve', '7dcd340d84f762eba80aa538b0c527f7', 1, 1),
-(7, 'Parth', 'Ladda', 'Parth001', 'path.ladda@gmail.com', '$2y$10$g8hFONlmBTi/5AYGiw.VOuXMDiY3AwC8Id9xDNwc.b62wW4bm3Pg.', 'e5f6ad6ce374177eef023bf5d0c018b6', 2, 0),
 (9, 'Sneha', 'Joglekar', 'snehaj', 'snehajoglekar123@gmail.com', '$2y$10$mAskjcEBp2lQTWe0dhih1epUBddThkQEiYC.JklcsJ3Rc0XxehF76', '1ff8a7b5dc7a7d1f0ed65aaa29c04b1e', 2, 1),
 (10, 'Anup', 'Chatlawar', 'Anup_321', 'anupcha2299@gmail.com', '$2y$10$wahcArvn8DBaAP1q4NIcbufnh05P.NPibAACoAxzW.Avc57wW4pKO', '94f6d7e04a4d452035300f18b984988c', 1, 1),
-(11, 'Francis', 'Lepcha', 'francislp21', 'francislp21@gmail.com', '$2y$10$7GZNfxMoUM4urCH/FvvDBeQH.pkQoXgqr50c1FVeRYqAdm.ya8Pvq', 'fbd7939d674997cdb4692d34de8633c4', 2, 1);
+(11, 'Francis', 'Lepcha', 'francislp21', 'francislp21@gmail.com', '$2y$10$7GZNfxMoUM4urCH/FvvDBeQH.pkQoXgqr50c1FVeRYqAdm.ya8Pvq', 'fbd7939d674997cdb4692d34de8633c4', 2, 1),
+(12, 'Rupash', 'Rauth', 'Rups_420', 'rupsr420@gmail.com', '$2y$10$xAmAg6/Q1lZ99S8fV4eMveboW6fgiWhQMecG.VwzAmOWo0erQFZ0y', '5dd9db5e033da9c6fb5ba83c7a7ebea9', 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -375,13 +393,13 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `stud_id` smallint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `stud_id` smallint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `test_attempted`
 --
 ALTER TABLE `test_attempted`
-  MODIFY `attempt_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `attempt_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `test_bank`
@@ -399,13 +417,13 @@ ALTER TABLE `test_questions`
 -- AUTO_INCREMENT for table `test_result`
 --
 ALTER TABLE `test_result`
-  MODIFY `result_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `result_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` smallint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `user_id` smallint(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
