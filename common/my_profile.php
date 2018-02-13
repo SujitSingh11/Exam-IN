ef453ae901dd8138af9086381d16dee179a62ac7
Sujit Singh <33032751+baby-developer@users.noreply.github.com>
2018-02-13 11:31:11 +0530

    my_profile added

--- common/my_profile.php
+++ common/my_profile.php
@@ -43,12 +43,47 @@
 					<h4 class="card-header display-4" style="font-size:30px;">My Profile</h4>
 					<div class="card-body">
 						<hr>
-						<div class="row">
-							<div class="col-md-4">
-								
+						<div class="col">
+							<div class="card mx-4">
+								<div class="card-body">
+									<div class="row mx-3">
+										<div class="col-md-3 col-sm-3">
+											<div id="profile" class="container">
+												<?php
+												if ($_SESSION['user_type']==0) {
+													$designation = "Admin";
+													echo"<img class='mb-2' src='../img/profile7.jpg' width='60%'>";
+												}
+												elseif ($_SESSION['user_type']==1) {
+													$designation = "Staff";
+													echo"<img id='img' class='mb-2 rounded-circle' src='../img/profile4.png' width='60%'>";
+												}
+												elseif ($_SESSION['user_type']==2) {
+													$designation = "Student";
+													echo"<img class='mb-2' src='../img/profile5.png' width='60%'>";
+												}
+												?>
+											</div>
+										</div>
+										<div class="offset-3 col-md-4 col-sm-4">
+											<h5 class="card-text my-2">Name:<i class="display-4" style="font-size:20px"> <?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?></i></h5>
+											<h5 class="card-text my-2">Designation:<i class="display-4" style="font-size:20px"> <?php echo $designation; ?></i></h5>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 						<hr>
+						<div class="col">
+							<div class="card mx-4">
+								<div class="card-body mx-3">
+									<h5 class="card-title display-4" style="font-size:30px;">Other Information:</h5>
+									<h5 class="card-text my-2">Email:<i class="display-4" style="font-size:20px"> <?php echo $_SESSION['email']; ?></i></h5>
+									<h5 class="card-text my-2">Username:<i class="display-4" style="font-size:20px"> <?php echo $_SESSION['username']; ?></i></h5>
+									<h5 class="card-text my-2">Designation:<i class="display-4" style="font-size:20px"> <?php echo $designation; ?></i></h5>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
             </div>
